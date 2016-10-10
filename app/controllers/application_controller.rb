c33124faf90a3bb387e21acf2a6054649e7aa205
Masaki Ando <masakiando@Masaki-no-MacBook-Pro.local>
2016-10-10 18:15:28 +0900

    error rescue500設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,21 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
- layout :set_layout
+  layout :set_layout
 
- private
+  rescue_from Exception, with: :rescue500
+
+  private
   def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
       Regexp.last_match[1]
     else
       'customer'
     end
+  end
+
+  def rescue500(e)
+    @exception = e
+    render 'errors/internal_server_error', status: 500
   end
 end
