d102b25cf5fe9a80d8af1feda666bcbefea999aa
Masaki Ando <masakiando@Masaki-no-MacBook-Pro.local>
2016-10-14 08:02:22 +0900

    ルーティング実装　リソース設定・ホスト機能設定・テスト実装

---
+++ spec/routing/hostname_constraints_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+describe 'ルーティング' do
+  example '職員トップページ' do
+    expect(get: 'http://baukis.example.com').to route_to(
+      host: 'baukis.example.com',
+      controller: 'staff/top',
+      action: 'index'
+    )
+  end
+
+  example '管理者ログインフォーム' do
+   expect(get: 'http://baukis.example.com/admin/login').to route_to(
+     host: 'baukis.example.com',
+     controller: 'admin/sessions',
+     action: 'new'
+   )
+  end
+
+  example 'ホスト名が対象外ならerrors/not_foundへ' do
+    expect(get: 'http://foo.example.jp').to route_to(
+      controller: 'errors',
+      action: 'not_found'
+    )
+  end
+
+  example '存在しないパスならerrors/not_foundへ' do
+    expect(get: 'http://example.com/xyz').to route_to(
+      controller: 'errors',
+      action: 'not_found',
+      anything: 'xyz'
+    )
+  end
+
+  describe 'ルーティング' do
+    example '顧客トップページ' do
+      expect(get: 'http://example.com/mypage').to route_to(
+        host: 'example.com',
+        controller: 'customer/top',
+        action: 'index'
+      )
+    end
+  end
+end
+# spec/routing/hostname_constraints_spec.rb
