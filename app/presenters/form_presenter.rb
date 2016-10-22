95f4d190855064572e532ccefbfa073f52d6bb06
Masaki Ando <masakiando@Masaki-no-MacBook-Pro.local>
2016-10-22 11:37:16 +0900

    ドロップダウンリスト生成

--- app/presenters/form_presenter.rb
+++ app/presenters/form_presenter.rb
@@ -3,7 +3,7 @@ class FormPresenter
 
   attr_reader :form_builder, :view_context
   delegate :label, :text_field, :password_field, :check_box, :radio_button,
-   :text_area, :object, to: :form_builder
+    :text_area, :object, to: :form_builder
 
   def initialize(form_builder, view_context)
     @form_builder = form_builder
@@ -47,6 +47,14 @@ class FormPresenter
     end
   end
 
+  def drop_down_list_block(name, label_text, choices, options = {})
+    markup(:div, class: 'input-block') do |m|
+      m << decorated_label(name, label_text, options)
+      m << form_builder.select(name, choices, { include_blank: true }, options)
+      m << error_messages_for(name)
+    end
+  end
+
   def error_messages_for(name)
     markup do |m|
       object.errors.full_messages_for(name).each do |message|
@@ -58,7 +66,7 @@ class FormPresenter
   end
 
   private
-    def decorated_label(name, label_text, options = {})
-      label(name, label_text, class: options[:required] ? 'required' : nil)
-    end
+  def decorated_label(name, label_text, options = {})
+    label(name, label_text, class: options[:required] ? 'required' : nil)
+  end
 end
