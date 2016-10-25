7fd6f8e19f805653030ecc02a332c4a9241b7cc2
Masaki Ando <masakiando@Masaki-no-MacBook-Pro.local>
2016-10-25 14:13:46 +0900

    共通コードモジュール化によるコード整理

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -1,7 +1,6 @@
 class Administrator < ActiveRecord::Base
-  before_validation do
-    self.email_for_index = email.downcase if email
-  end
+  include EmailHolder
+
   def password=(raw_password)
     if raw_password.kind_of?(String)
       self.hashed_password = BCrypt::Password.create(raw_password)
