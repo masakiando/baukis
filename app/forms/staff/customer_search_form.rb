9ea209ce0b31a5b04b37b788be70be8d6472c433
Masaki Ando <masakiando@Masaki-no-MacBook-Pro.local>
2016-10-29 17:01:09 +0900

    検索条件の追加、検索値の正規化

--- app/forms/staff/customer_search_form.rb
+++ app/forms/staff/customer_search_form.rb
@@ -1,11 +1,15 @@
 class Staff::CustomerSearchForm
   include ActiveModel::Model
+  include StringNormalizer
 
   attr_accessor :family_name_kana, :given_name_kana,
     :birth_year, :birth_month, :birth_mday,
-    :address_type, :prefecture, :city, :phone_number
+    :address_type, :prefecture, :city, :phone_number,
+    :page
 
   def search
+    normalize_values
+
     rel = Customer
     if family_name_kana.present?
       rel = rel.where(family_name_kana: family_name_kana)
@@ -17,6 +21,35 @@ class Staff::CustomerSearchForm
     rel = rel.where(birth_month: birth_month) if birth_month.present?
     rel = rel.where(birth_mday: birth_mday) if birth_mday.present?
 
-    rel.order(:family_name_kana, :given_name_kana)
+    if prefecture.present? || city.present?
+      case address_type
+      when 'home'
+        rel = rel.joins(:home_address)
+      when 'work'
+        rel = rel.joins(:work_address)
+      when ''
+        rel = rel.joins(:addresses)
+      else
+        raise
+      end
+      if prefecture.present?
+        rel = rel.where('addresses.prefecture' => prefecture)
+      end
+      rel = rel.where('addresses.city' => city) if city.present?
+    end
+
+    if phone_number.present?
+      rel = rel.joins(:phones).where('phones.number_for_index' => phone_number)
+    end
+    rel.order(:family_name_kana, :given_name_kana).page(page)
+  end
+
+  private
+  def normalize_values
+    self.family_name_kana = normalize_as_furigana(family_name_kana)
+    self.given_name_kana = normalize_as_furigana(given_name_kana)
+    self.city = normalize_as_name(city)
+    self.phone_number = normalize_as_phone_number(phone_number)
+      .try(:gsub, /\D/, '')
   end
 end
