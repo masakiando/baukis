d58d220874c25d0114bb95a7da1c5cc9bd633f19
Masaki Ando <masakiando@Masaki-no-MacBook-Pro.local>
2016-11-07 11:21:53 +0900

    プログラムルートへ新アクションentriesの設定。新アクションのentries定義。 エントリー有りプログラム時のdestroyアクション定義

--- app/controllers/staff/programs_controller.rb
+++ app/controllers/staff/programs_controller.rb
@@ -39,4 +39,24 @@ class Staff::ProgramsController < Staff::Base
       render action: 'edit'
     end
   end
+
+  def destroy
+    program = Program.find(params[:id])
+    if program.deletable?
+      program.destroy!
+      flash.notice = 'プログラムを削除しました。'
+    else
+      flash.alert = 'このプログラムは削除できません。'
+    end
+    redirect_to :staff_programs
+  end
+
+  # PATCH
+  def entries
+    entries_form = Staff::EntriesForm.new(Program.find(params[:id]))
+    entries_form.assign_attributes(params[:form])
+    entries_form.save
+    flash.notice = 'プログラム申し込みのフラグを更新しました。'
+    redirect_to :staff_programs
+  end
 end
