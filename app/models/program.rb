8cda458322a0562ac77f61e19c01393367e8f943
Masaki Ando <masakiando@Masaki-no-MacBook-Pro.local>
2016-11-07 10:52:05 +0900

    各属性に対しバリデーションの設定。カスタムバリデーターの読み込み。エントリー済みプログラム削除アクション時の設定。

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,5 +1,5 @@
 class Program < ActiveRecord::Base
-  has_many :entries, dependent: :destroy
+  has_many :entries, dependent: :restrict_with_exception
   has_many :applicants, through: :entries, source: :customer
   belongs_to :registrant, class_name: 'StaffMember'
 
@@ -13,6 +13,35 @@ class Program < ActiveRecord::Base
   before_validation :set_application_start_time
   before_validation :set_application_end_time
 
+  validates :title, presence: true, length: { maximum: 32 }
+  validates :description, presence: true, length: { maximum: 800 }
+  validates :application_start_date,
+            :application_end_date, date_string: true
+  validates :application_start_time, date: {
+               after_or_equal_to: Time.zone.local(2000, 1, 1),
+               before: -> (obj) { 1.year.from_now },
+               allow_blank: true
+             }
+  validates :application_end_time, date: {
+               after: :application_start_time,
+               before: -> (obj) { obj.application_start_time.advance(days: 90) },
+               allow_blank: true,
+               if: -> (obj) { obj.application_start_time }
+             }
+  validates :min_number_of_participants, numericality: {
+             only_integer: true, greater_than_or_equal_to: 1,
+             less_than_or_equql_to: 1000, allow_blank: true }
+
+  validates :max_number_of_participants, numericality: {
+            only_integer: true, greater_than_or_equal_to: 1,
+            less_than_or_equql_to: 1000, allow_blank: true }
+
+  validate do
+    if min_number_of_participants && max_number_of_participants &&
+      min_number_of_participants > max_number_of_participants
+      errors.add(:max_number_of_participants, :less_than_min_number)
+    end
+  end
 
   scope :listing, -> {
     joins('LEFT JOIN entries ON programs.id = entries.program_id')
@@ -22,7 +51,12 @@ class Program < ActiveRecord::Base
       .includes(:registrant)
   }
 
+  def deletable?
+    entries.empty?
+  end
+
   private
+  # データ加工:日付確認、０時にセット、時、分を入力値へ進める
   def set_application_start_time
     return if application_start_date.blank?
     Date.parse(application_start_date)
