bdb4f7340ed1543877ffe4d080d496cea3add58f
Masaki Ando <masakiando@Masaki-no-MacBook-Pro.local>
2016-11-20 06:26:56 +0900

    js及びcssにtag-itファイルの読み込み設定

--- app/assets/javascripts/staff.js
+++ app/assets/javascripts/staff.js
@@ -1,7 +1,8 @@
 //= require jquery
 //= require jquery_ujs
 // require turbolinks #アウト中
-//= require jquery.ui.datepicker
+//= require jquery.ui.all
 //= require jquery.ui.datepicker-ja
+//= require tag-it
 //= require_tree ./shared
 //= require_tree ./staff
